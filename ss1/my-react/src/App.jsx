b130317bdb7c06252130c5297edfbeaef08e45da
triet2203 <minhtriet2203@gmail.com>
2026-08-28 11:46:44 +0700

    update bai tap

--- ss1/my-react/src/App.jsx
+++ ss1/my-react/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import CityList from "./lessons/ss1_ReactJs/CityList.jsx";
+import Element from "./lessons/ss1_ReactJs/Element.jsx";
 
 function App() {
     return (
         <div>
             <h1>Hello ReactJS</h1>
             <p>Đây là ví dụ về JSX</p>
-            <CityList/>
         </div>
     );
 }
--- ss1/my-react/src/main.jsx
+++ ss1/my-react/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Element from "./lessons/ss1_ReactJs/Element.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Element />
   </StrictMode>,
 )
