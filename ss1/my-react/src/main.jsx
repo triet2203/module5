a87b0c1014cf1ee37b1b6d1a0cedd55fa01a2be9
triet2203 <minhtriet2203@gmail.com>
2026-09-03 14:42:21 +0700

    bai tap sign in form

--- ss1/my-react/src/main.jsx
+++ ss1/my-react/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Element from "./lessons/ss1_ReactJs/Element.jsx";
+import SignInForm from "./lessons/ss1_ReactJs/SignInForm.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Element />
+    <SignInForm />
   </StrictMode>,
 )
